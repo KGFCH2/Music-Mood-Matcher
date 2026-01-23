3ed21df247a066c62ace39eb4203baae4d421b7f
KGFCH2 <babinbid05@gmail.com>
2026-01-23 20:53:42 +0530

    Add Jwt Auth Middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,16 +2,16 @@ const jwt = require('jsonwebtoken')
 const JWT_SECRET = process.env.JWT_SECRET || 'devsecret'
 
 function authMiddleware(req, res, next) {
-  const auth = req.headers.authorization
-  if (!auth || !auth.startsWith('Bearer ')) return res.status(401).json({ message: 'Unauthorized' })
-  const token = auth.split(' ')[1]
-  try {
-    const payload = jwt.verify(token, JWT_SECRET)
-    req.user = payload
-    next()
-  } catch (err) {
-    res.status(401).json({ message: 'Invalid token' })
-  }
+    const auth = req.headers.authorization
+    if (!auth || !auth.startsWith('Bearer ')) return res.status(401).json({ message: 'Unauthorized' })
+    const token = auth.split(' ')[1]
+    try {
+        const payload = jwt.verify(token, JWT_SECRET)
+        req.user = payload
+        next()
+    } catch (err) {
+        res.status(401).json({ message: 'Invalid token' })
+    }
 }
 
 module.exports = { authMiddleware }
