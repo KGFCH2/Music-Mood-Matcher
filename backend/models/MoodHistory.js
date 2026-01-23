e656dbc9c04fe6c8add291978f517defc3d70054
KGFCH2 <babinbid05@gmail.com>
2026-01-23 20:54:18 +0530

    Add Mood History Model

--- backend/models/MoodHistory.js
+++ backend/models/MoodHistory.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const moodHistorySchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  mood: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now }
+    userId: { type: String, required: true },
+    mood: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now }
 })
 
 module.exports = mongoose.model('MoodHistory', moodHistorySchema)
